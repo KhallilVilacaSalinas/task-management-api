007a87a59e4245111d97f70c5b06cfb9bc5c8812
vilaca <khallil.vilaca@gmail.com>
2024-07-31 10:46:25 -0300

    feat: creating endpoints for update, delele, find all, find by id

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,13 +1,35 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { TaskDto } from './task.dto';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
 import { TaskService } from './task.service';
 
 @Controller('task')
 export class TaskController {
+
 	constructor(private readonly taskService: TaskService) {}
 
 	@Post()
 	create(@Body() task: TaskDto) {
-		console.log(task);
+		this.taskService.create(task);
 	}
+
+    @Get('/:id')
+    findById(@Param('id') id: string) {
+        console.log(this.taskService.findById(id))
+    }
+
+    @Get()
+    findAll(@Query() params: FindAllParameters): TaskDto[] {
+        return this.taskService.findAll(params);
+    }
+
+    @Put()
+    update(@Body() task: TaskDto) {
+        this.taskService.update(task);
+    }
+
+    @Delete('/:id')
+    remove(@Param('id') id: string) {
+        return this.taskService.remove(id)
+    }
+
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@nestjs/common';
-import { TaskDto } from './task.dto';
+import { HttpException, HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
+import { FindAllParameters, TaskDto } from './task.dto';
 
 @Injectable()
 export class TaskService {
@@ -8,4 +8,52 @@ export class TaskService {
 	create(task: TaskDto) {
 		this.tasks.push(task);
 	}
+
+    findById(id: string): TaskDto {
+        const task = this.tasks.filter(item => (item.id === id));
+
+        if (task.length) {
+            return task[0]
+        }
+
+        throw new NotFoundException(`Task with id ${id} not found`);
+    }
+
+    findAll(params: FindAllParameters): TaskDto[] {
+        return this.tasks.filter(t => {
+            let match = true;
+
+            if (params.title != undefined && !t.title.includes(params.title)) {
+                match = false;
+            }
+
+            if (params.status != undefined && !t.status.includes(params.status)) {
+                match = false;
+            }
+
+            return match;
+        })
+    }
+    
+    update(task: TaskDto) {
+        let taskIndex = this.tasks.findIndex(t => t.id === task.id);
+
+        if (taskIndex >= 0) {
+            this.tasks[taskIndex] = task
+            return;
+        }
+
+        throw new HttpException('TASK_ID_NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
+
+    remove(id: string) {
+        let taskIndex = this.tasks.findIndex(t => t.id === id);
+
+        if (taskIndex >= 0) {
+            this.tasks.splice(taskIndex, 1);
+            return;
+        }
+
+        throw new HttpException('TASK_ID_NOT_FOUND', HttpStatus.NOT_FOUND);
+    }
 }
